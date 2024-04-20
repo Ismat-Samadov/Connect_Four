8e56ecd9ae78a47a5b3bd10939e933928971323b
Ismat-Samadov <ismetsemedov@gmail.com>
2024-04-20 18:49:53 +0400

    connect_four.rb and connect_four_spec.rb are created

---
+++ connect_four.rb
@@ -0,0 +1,58 @@
+# connect_four.rb
+
+class ConnectFour
+  attr_reader :board
+
+  def initialize
+    @board = Array.new(6) { Array.new(7, ' ') }
+  end
+
+  def drop_piece(column, piece)
+    row = find_empty_row(column)
+    return false unless row
+
+    @board[row][column] = piece
+    true
+  end
+
+  def check_win(piece)
+    return true if horizontal_win?(piece)
+    return true if vertical_win?(piece)
+    return true if diagonal_win?(piece)
+
+    false
+  end
+
+  private
+
+  def find_empty_row(column)
+    @board.reverse_each do |row|
+      return row if row[column] == ' '
+    end
+    nil
+  end
+
+  def horizontal_win?(piece)
+    @board.each do |row|
+      (0..3).each do |i|
+        return true if row[i..i + 3].all? { |cell| cell == piece }
+      end
+    end
+    false
+  end
+
+  def vertical_win?(piece)
+    (0..6).each do |column|
+      (0..2).each do |i|
+        return true if @board[i][column] == piece && @board[i + 1][column] == piece &&
+                        @board[i + 2][column] == piece && @board[i + 3][column] == piece
+      end
+    end
+    false
+  end
+
+  def diagonal_win?(piece)
+    # Implement diagonal win check here
+    false
+  end
+end
